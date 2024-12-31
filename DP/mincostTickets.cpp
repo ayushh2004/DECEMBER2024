5980f182d39ef656968f2f29ef986736a557f23b
ayushh2004 <22103228@mail.jiit.ac.in>
2024-12-31 12:16:48 +0530

    Update mincostTickets.cpp

--- DP/mincostTickets.cpp
+++ DP/mincostTickets.cpp
@@ -31,3 +31,29 @@ public:
         return solve(0,days,costs);
     }
 };
+
+
+
+
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n=days.size();
+        unordered_set<int> st(begin(days),end(days));
+        int lastDay=days.back();
+        vector<int> dp(366);
+        dp[0]=0;
+        for(int i=1;i<=lastDay;i++){
+            if(st.find(i)==st.end()){
+                dp[i]=dp[i-1];
+                continue;
+            }
+            dp[i]=INT_MAX;
+            int c1=costs[0]+dp[max(i-1,0)];
+            int c2=costs[1]+dp[max(i-7,0)];
+            int c3=costs[2]+dp[max(i-30,0)];
+            dp[i]=min({c1,c2,c3});
+        }
+        return dp[lastDay];
+    }
+};
